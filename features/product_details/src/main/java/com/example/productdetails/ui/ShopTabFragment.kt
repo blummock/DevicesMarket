2932a3c45da9d4604ab61f0745ac7fbc69ec9614
mad7m <mad7mile@gmail.com>
2023-04-20 10:27:51 +0800

    refactoring device info layout

--- features/product_details/src/main/java/com/example/productdetails/ui/ShopTabFragment.kt
+++ features/product_details/src/main/java/com/example/productdetails/ui/ShopTabFragment.kt
@@ -7,18 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.productdetails.databinding.ShopTabFragmentBinding
+import com.example.productdetails.databinding.ShopTabItemBinding
 
 class ShopTabFragment(private val viewModel: ProductDetailsViewModel) : Fragment() {
 
-    lateinit var binding: ShopTabFragmentBinding
+    lateinit var binding: ShopTabItemBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = ShopTabFragmentBinding.inflate(inflater)
+        binding = ShopTabItemBinding.inflate(inflater)
         return binding.root
     }
 
